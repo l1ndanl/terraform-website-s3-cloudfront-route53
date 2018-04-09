d5640ae7dd8a8fae58ed9e6a6b51cfa786ed1930
Tim van der Leeuw <tim@l1nda.nl>
2018-04-09 12:58:20 +0200

    LC-78 No custom origin

--- site-main/main.tf
+++ site-main/main.tf
@@ -99,13 +99,6 @@ resource "aws_cloudfront_distribution" "website_cdn" {
     s3_origin_config {
       origin_access_identity = "${aws_cloudfront_origin_access_identity.orig_access_ident.cloudfront_access_identity_path}"
     }
-
-    custom_origin_config {
-      origin_protocol_policy = "http-only"
-      http_port              = "80"
-      https_port             = "443"
-      origin_ssl_protocols   = ["TLSv1"]
-    }
   }
 
   default_root_object = "index.html"
